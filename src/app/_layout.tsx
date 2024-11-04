4e1337fa095c81a6aeda73aefdc9c0692f260e59
Sergio D. Barrera <sdbeng@gmail.com>
2024-11-03 21:50:20 -0800

    update classnames

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
 import "../../global.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
   return (
+    <QueryClientProvider client={queryClient}>     
     <Stack>
         <Stack.Screen name="index" options={{ title: "Home" }}/>
     </Stack>
+    </QueryClientProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,29 @@
 import { StatusBar, Text, View, StyleSheet, FlatList } from "react-native";
 import { thinkspaces } from "../data";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { useQuery } from "@tanstack/react-query";
 
 const thinkspaceData = thinkspaces[0];// it will be the first thinkspace object
 
 //define a function to display the thinkspace data
 function ThinkspaceListItem(props) {    
-    // console.log('props.thinkspaceData:', props.thinkspaceData);
+    // console.log('props.thinkspaceData:', props.thinkspaceData);    
+    const {title, duration} = props.thinkspaceData;
+    // console.log('title:', title);    
+
+    const {data, error, isLoading} = useQuery({queryKey: ['thinkData'], queryFn: () => fetch('data')});
+    if (isLoading) return <Text>Loading...</Text>
+    console.log('tanstck data:', data);
     
-    const {title} = props.thinkspaceData;
-    console.log('title:', title);    
 
     return (
         <View className="p-5 border-2 border-gray-300 rounded-2xl">
-            <Text className="font-semibold text-2xl">{title}</Text>      
+            <Text className="font-semibold text-2xl mb-2">{"test title"}</Text>
+            <View className="flex-row items-center gap-2">
+            <FontAwesome6 name="clock" size={16} color="#6B7280" />
+            <Text className="text-gray-500">{"test duration"} min</Text>    
+
+            </View>
         </View>
     )
 }
